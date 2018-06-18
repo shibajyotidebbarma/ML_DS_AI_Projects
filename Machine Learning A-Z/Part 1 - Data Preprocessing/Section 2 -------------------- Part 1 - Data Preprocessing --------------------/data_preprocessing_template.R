00b98b949f0ec11dc91dc3efaa0ca6299ba80e24
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-06-18 12:46:27 +0530

    Creating the Complete Data Preprocessing Template for projects in Python and R Scripts

--- Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -1,33 +1,38 @@
+# 
+# #Taking care of missing data
+# 
+# dataset$Age = ifelse(is.na(dataset$Age),
+#                      ave(dataset$Age, FUN = function(x) mean(x, na.rm=TRUE)),
+#                      dataset$Age)
+# dataset$Salary = ifelse(is.na(dataset$Salary),
+#                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm=TRUE)),
+#                      dataset$Salary)
+# 
+# #Encoding categorical data
+# dataset$Country = factor(dataset$Country, 
+#                          levels = c('France', 'Spain', 'Germany'),
+#                          labels =c(1,2,3)
+#                          )
+# dataset$Purchased = factor(dataset$Purchased,
+#                            levels = c('No', 'Yes'),
+#                            labels = c(0,1)
+#                            )
+
+
+
+
+
+
 
 #Data Preprocessing
 
 dataset = read.csv('Data.csv')
-
-#Taking care of missing data
-
-dataset$Age = ifelse(is.na(dataset$Age),
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm=TRUE)),
-                     dataset$Age)
-dataset$Salary = ifelse(is.na(dataset$Salary),
-                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm=TRUE)),
-                     dataset$Salary)
-
-#Encoding categorical data
-dataset$Country = factor(dataset$Country, 
-                         levels = c('France', 'Spain', 'Germany'),
-                         labels =c(1,2,3)
-                         )
-dataset$Purchased = factor(dataset$Purchased,
-                           levels = c('No', 'Yes'),
-                           labels = c(0,1)
-                           )
+#dataset = dataset[, 2:3]
 
 #Splitting the dataset into Training set and Test Set
 #install.packages('caTools')
 
 library(caTools)
-
-
 set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
@@ -35,9 +40,9 @@ test_set = subset(dataset, split == FALSE)
 
 
 
-#Feature Scaling
-training_set[, 2:3] = scale(training_set[, 2:3])
-test_set[, 2:3]= scale(test_set[, 2:3])
+# #Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3]= scale(test_set[, 2:3])
 
 
 
