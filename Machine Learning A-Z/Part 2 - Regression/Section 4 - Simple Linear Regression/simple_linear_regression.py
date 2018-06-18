7a693ed4bec2a5fea0c27aa9fa575b52cb6225b0
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-06-18 22:52:26 +0530

    Visualising the Training and Test Sets in Python Script Added

--- Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -15,7 +15,6 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=1/3, random_state=0)
 
-
 #Feature Scaling
 """
 from sklearn.preprocessing import StandardScaler
@@ -29,17 +28,24 @@ from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
-
 #Predicting the Test Set Results
-
-y_pred = regressor.predict(X_train)
+y_pred = regressor.predict(X_test)
  
+#Visualising the Training Set Results
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience(Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
 
-
-
-
-
-
+#Visualising the Test Set Results
+plt.scatter(X_test, y_test, color="red")
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience(Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
 
 
 
