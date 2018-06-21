7f2a79a6d60344c64445200216a918b1aabfd73a
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-06-21 20:20:18 +0530

    Building the optimal model using Backward Elimination in Python Script is added

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -56,7 +56,10 @@ regressor.fit(X_train, y_train)
 #Predicting the Test Set Results
 y_pred = regressor.predict(X_test)
 
+#Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
 
+X = np.append(arr=np.ones((50,1)).astype(int), values=X, axis = 1)
 
 
 
