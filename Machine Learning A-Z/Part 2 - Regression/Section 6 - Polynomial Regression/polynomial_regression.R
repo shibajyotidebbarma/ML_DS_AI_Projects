af6de3d373580425d60b5a18c4b9e51ecdebd5c6
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-06-27 20:26:40 +0530

    Add dataset

--- Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
@@ -1,1 +1,21 @@
 # Polynomial Regression
+
+#Data Preprocessing
+
+dataset = read.csv('Position_Salaries.csv')
+
+
+#Splitting the dataset into Training set and Test Set
+#install.packages('caTools')
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+
+
+# #Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3]= scale(test_set[, 2:3])
