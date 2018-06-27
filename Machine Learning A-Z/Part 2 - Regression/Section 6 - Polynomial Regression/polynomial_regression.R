3fb3d3d6bcc16bba4f432e1f6fe080164130bc53
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-06-27 20:42:35 +0530

    Fit Polynomial Regression to the dataset

--- Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
@@ -3,19 +3,69 @@
 #Data Preprocessing
 
 dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Splitting the dataset into Training set and Test Set
+# install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
 
 
-#Splitting the dataset into Training set and Test Set
-#install.packages('caTools')
 
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
-
-
-
-# #Feature Scaling
+# Feature Scaling
 # training_set[, 2:3] = scale(training_set[, 2:3])
 # test_set[, 2:3]= scale(test_set[, 2:3])
+
+
+# Fitting Linear Regression to the dataset
+
+lin_reg = lm(formula = Salary ~ .,
+             data = dataset)
+
+# run summary(lin_reg) on command after executing the above line
+
+
+
+# Fitting Polynomial Regression to the dataset
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+poly_reg = lm(formula = Salary ~ .,
+              data = dataset)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
