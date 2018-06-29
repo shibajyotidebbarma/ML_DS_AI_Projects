675faf2184dd2c81a80722eb9fec8d30810874f8
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-06-29 18:32:10 +0530

    Implement SVR to build the Truth or Bluff Predication Model in python

--- Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
@@ -16,18 +16,21 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+X = sc_X.fit_transform(X)
+y = y.reshape(-1,1)
+y = sc_y.fit_transform(y)
 
 # Fitting the Regression Model to the dataset
-# Create your regressor here
+from sklearn.svm import SVR
+regressor = SVR(kernel ='rbf')
+regressor.fit(X,y)
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+
 
 # Visualising the Regression results
 plt.scatter(X, y, color = 'red')
