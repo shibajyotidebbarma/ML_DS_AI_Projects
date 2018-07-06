9b91660f835d8e2ad8edc464c25dd0953427d2d9
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-07-06 14:44:28 +0530

    Fit the Classifier to the dataset for Logistic Regression

--- Machine Learning A-Z/Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression.py
+++ Machine Learning A-Z/Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression.py
@@ -19,3 +19,8 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+#Fitting Logistic Regression to the Training Set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+
