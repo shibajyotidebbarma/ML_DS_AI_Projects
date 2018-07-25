3a96585a718873c580b542b56ee0068e10accb5e
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-07-25 15:00:24 +0530

    Imported the mall dataset using pandas from the Mall_Customers CSV file

--- Machine Learning A-Z/Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
+++ Machine Learning A-Z/Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
@@ -6,3 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+#Importing the mall dataset with pandas
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3,4]].values
+
+
+
