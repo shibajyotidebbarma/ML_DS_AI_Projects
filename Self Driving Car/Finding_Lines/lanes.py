d81ab64d4a6e472f17196ca1054ea96e74055cdd
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2019-04-13 23:54:25 +0530

    Implemented code to identify lines in a gradient image using Hough Transform Technique

--- Self Driving Car/Finding_Lines/lanes.py
+++ Self Driving Car/Finding_Lines/lanes.py
@@ -11,6 +11,15 @@ def canny(image):
     return canny
 
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
+
+
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([
@@ -18,13 +27,19 @@ def region_of_interest(image):
     ])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    return mask
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 
 canny = canny(lane_image)
+cropped_image = region_of_interest(canny)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180,
+                        100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
 
-cv2.imshow('result', region_of_interest(canny))
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow('result', combo_image)
 cv2.waitKey(0)
